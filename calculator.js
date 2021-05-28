22f4da60df282a34fe484954da7611abdf55366e
Bryan <bryan.how@live.com>
2021-05-28 21:35:27 +0800

    to resolve NaN when displaying results

--- calculator.js
+++ calculator.js
@@ -1,3 +1,6 @@
+let inputValue = [];
+let inputOperator;
+
 // basic functions for the calculator
 function add(a, b) {
     return a + b;
@@ -18,18 +21,20 @@ function divide(a, b) {
 
 // function that takes an operator and 2 numbers and then calls one of the above functions
 function operate(opt, num1, num2) {
+    let n1 = Number(num1);
+    let n2 = Number(num2);
     switch(opt) {
         case "+":
-            return add(num1, num2);
+            return add(n1, n2);
         
         case "-":
-            return subtract(num1, num2);
+            return subtract(n1, n2);
         
         case "*":
-            return multiply(num1, num2);
+            return multiply(n1, n2);
         
         case "/":
-            return divide(num1, num2);
+            return divide(n1, n2);
     }; 
 };
 
@@ -37,13 +42,35 @@ function operate(opt, num1, num2) {
 // function that populate the display when number buttons are pressed (will populate in the mini-display)
 function populateMiniDisplay(input) {
     let miniDisplay = document.querySelector("#mini-display");
-    let inputValue = input;
     miniDisplay.textContent += input;
 };
 
 const buttons = document.querySelectorAll(".calculator-button");
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        populateMiniDisplay(button.value)
+        inputValue.push(button.value);
+        populateMiniDisplay(button.value);
     });
+});
+
+const operatorButtons = document.querySelectorAll("#operator");
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        inputOperator = button.value;
+    });
+})
+
+
+// show the results of the calculator on the main display (#display)
+function populateDisplay(result) {
+    let display = document.querySelector("#display");
+    display.textContent += result;
+};
+
+let equalButton = document.querySelector("#equal");
+equalButton.addEventListener("click", () => {
+    let res = operate(inputOperator, inputValue[0], inputValue[1]);
+    inputValue.splice(0, 2);
+    inputOperator = "";
+    return populateDisplay(res);
 });
